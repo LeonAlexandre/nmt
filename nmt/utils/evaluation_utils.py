Fixed data problems and changed metrics calculation.
Fixed the newline concatenating problem in some data files.
Changed the metrics calculation to normalize against the label file internally.

nmt/utils/evaluation_utils.py
@@ -74,7 +74,7 @@ def _clean(sentence, subword_option):
 def _edit_distance(ref_file, predict_file):
 
   '''
-  Computes the edit_distance averaged over the total number of sequences in the files
+  Computes the normalized edit_distance averaged over the total number of sequences in the files
   Returns the negative of the average distance to be compatible with the code base, specifically trying to maximize a given metric
   '''
 
@@ -90,7 +90,9 @@ def _edit_distance(ref_file, predict_file):
           predict_line = predict_line.strip()
           ref_line = ref_line.replace(" ","")
           predict_line = predict_line.replace(" ","")
-          ed_dis += lv.distance(ref_line,predict_line)
+          norm_ed = lv.distance(ref_line,predict_line) / len(ref_line)
+          ed_dis += norm_ed
+
           #print("Label: " + ref_line)
           #print("Prediction: " + predict_line)
 
@@ -101,8 +103,10 @@ def _edit_distance(ref_file, predict_file):
 def _hamming_distance(ref_file, predict_file):
 
   '''
-  Computes the hamming distance average over the total number of sequences in the files
-  Because the hamming distance is not defined for sequences of different lengths
+  For every sequence, compute the normalized hamming distance.
+  Return normalized hamming distance averaged over all the sequences
+  Because the hamming distance is not defined for sequences of different lengths,
+  padding and truncating is used.
   '''
 
   with codecs.getreader("utf-8")(tf.gfile.GFile(ref_file,"rb")) as ref_fh:
@@ -139,7 +143,7 @@ def _hamming_distance(ref_file, predict_file):
             print("Network output shorter than target on line #%d, padding to target length with \'1\'" % line_number)
             num_printed += 1
 
-        hdis += sum(predict_sym != ref_sym for (predict_sym, ref_sym) in zip(predict_line, ref_line))
+        hdis += sum(predict_sym != ref_sym for (predict_sym, ref_sym) in zip(predict_line, ref_line)) / len_ref
     
   avg = -1 * hdis / num_lines
   return avg